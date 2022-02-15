a84a948ff7c88f9ec05cd7673b0fa3755612414d
wing gayongan <w.gayongan@gmail.com>
2022-02-15 20:54:26 +0800

    fking bug fixed

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,76 +2,96 @@ const socket = io();
 const kanji = document.querySelector("h1");
 const form = document.querySelector("form");
 const romaji = form.querySelector("div:first-child input");
+const rData = form.querySelector("div:first-child h3");
 const english = form.querySelector("div:nth-child(2) input");
-const submit = form.querySelector("button");
+const eData = form.querySelector("div:nth-child(2) h3")
+const submit = form.querySelector("div:nth-child(3) input");
+const next = form.querySelector("div:nth-child(3) button");
+let romaji_value;
+let eng_value
 
 
-romaji.setAttribute("required","");
-english.setAttribute("required","");
+// modifying/hidind the elements
+next.innerHTML = "&#10095";
+next.style.display = "none";
+
+
+//socket.io eventslisteners
 socket.on("play", (msg) => {
     
     kanji.innerHTML = `${msg}`;
 })
 
-submit.addEventListener("click", (event) => {
-    event.preventDefault();
-    const romaji_value = romaji.value;
-    const english_value = english.value;
-
-    socket.emit("submit", 
-        romaji_value, 
-        english_value,
-        () => {
-            submit.remove();
-
-            const div = form.querySelector("div:nth-child(3)");
-            next = document.createElement("button");
-            next.innerText = "Next";
-
-            div.appendChild(next);
-        }
-    );
-});
-
-
-socket.on("roErr", (msg) => {
-    romaji.remove();
-    const div = form.querySelector("div:first-child");
+socket.on("romErr", (msg) =>{
     const h3 = document.createElement("h3");
     
     h3.innerHTML = `${msg}❌`;
-    
-    div.appendChild(h3);
-    
+    romaji.parentNode.replaceChild(h3, romaji);
+
 });
 
-socket.on("rCorrect", (msg) => {
-    
-    romaji.remove();
-    const div1 = form.querySelector("div:first-child");
-    const h3 = document.createElement("h3");    
+socket.on("rCorrect", (msg) =>{
+    const h3 = document.createElement("h3");
+
     h3.innerHTML = `${msg}✔`;
-
-    div1.appendChild(h3);
-});
-
-socket.on("eError", (msg) => {
-    english.remove();
-    const div = form.querySelector("div:nth-child(2)")
-    const h3 = document.createElement("h3");
-    h3.innerHTML = `${msg}❌`;
-
-    div.appendChild(h3);
-    
+    romaji.parentNode.replaceChild(h3, romaji);
 });
 
 socket.on("eCorrect", (msg) =>{
-    
-    english.remove();
-    const div = form.querySelector("div:nth-child(2)");
     const h3 = document.createElement("h3");
 
     h3.innerHTML = `${msg}✔`;
-    div.appendChild(h3);
-    console.log("correct");
-})
+    english.parentNode.replaceChild(h3, english);
+
+});
+
+socket.on("eError", (msg) => {
+    const h3 = document.createElement("h3");
+
+    h3.innerHTML = `${msg}❌`;
+    english.parentNode.replaceChild(h3, english);
+
+});
+
+
+// event Handlers
+function submitHandler(event){
+    
+    event.preventDefault();
+    romaji_value = romaji.value;
+    eng_value = english.value;
+
+    socket.emit("submit", 
+                romaji_value,
+                eng_value,
+                () => {
+                    submit.style.display = "none";
+                    next.style.display =  "block"
+                }
+
+    );
+
+    romaji_value = "";
+    eng_value = "";
+
+};
+
+function enterHandler(event){
+    if (event.keycode === 13){
+        form.submit();
+    }
+    
+}
+
+function nextHandler(event){
+    event.preventDefault();
+    next.style.color = "red";
+    console.log("next...")
+};
+
+
+
+// event listeners
+form.addEventListener("submit", submitHandler);
+form.addEventListener("keyup", enterHandler);
+next.addEventListener("click", nextHandler);
--- src/server.js
+++ src/server.js
@@ -2,19 +2,14 @@ import {createServer} from "http";
 import {Server} from "socket.io"
 import express from "express";
 import path from "path/posix";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 import kanji from "./models/kanji.js";
 
 const app = express();
 const port = 3000;
 const httpServer = createServer(app);
 const ioServer = new Server(httpServer);
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 let data;
 let data_kanji;
-
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
@@ -30,24 +25,27 @@ app.get("/*", (req, res) => {
     res.redirect("/")
 });
 
-const start = () => {
+function start(){
 
     data = kanji[Math.floor(Math.random() * kanji.length + 1)];
     data_kanji = data.japanese;
+
+    return data_kanji
 }
 
 ioServer.on("connection", (socket) => {
     
     start();
-    socket.emit("play", data_kanji)
-
-    socket.on("submit", (rom, eng, play) =>{
-
-        if(rom = !data.romaji){
-
-            socket.emit("roErr", data.romaji);
+    socket.emit("play", start());
+    socket.on("submit", (rom, eng, next) =>{
+        
+        
+        next();
+        if(rom != data.romaji){
+            
+            socket.emit("romErr", data.romaji);
         }
-        if(rom = data.romaji){
+        if(rom === data.romaji){
             socket.emit("rCorrect", data. romaji);
         }
         if(data.english.includes(eng)){
@@ -56,15 +54,11 @@ ioServer.on("connection", (socket) => {
         if(!data.english.includes(eng)){
             socket.emit("eError", data.english);
         }
-        
-        play();
     });
     
     console.log(data.romaji);
     console.log(data.english);
-    socket.on("next", () =>{
-        start();
-    });
-
 });
+
+
 httpServer.listen(port,() => console.log("running..."));
